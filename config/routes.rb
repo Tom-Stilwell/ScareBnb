1f4206862e47a14cf16eda622aac2bb9f23ced99
Tom_Stilwell <ThomasRStilwell@gmail.com>
2018-03-06 15:35:31 -0500

    Create routes for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create, :show, :update, :destroy]
+    resource :session, only: [:create, :destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "static_pages#root"
 end
